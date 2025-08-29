a2372105a273915b048a28aa72b40fb67cf95b5a
GBTNetwork <114756890+alexwambu@users.noreply.github.com>
2025-08-29 18:23:08 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+{
+  "api": {
+    "id": null,
+    "worker-id": "render-miner"
+  },
+  "autosave": true,
+  "background": false,
+  "colors": true,
+  "randomx": {
+    "1gb-pages": false,
+    "rdmsr": true,
+    "wrmsr": true,
+    "numa": false
+  },
+  "cpu": {
+    "enabled": true,
+    "huge-pages": false,
+    "hw-aes": null,
+    "priority": null,
+    "asm": true,
+    "max-threads-hint": 25,
+    "yield": true
+  },
+  "pools": [
+    {
+      "url": "pool.supportxmr.com:3333",
+      "user": "44WVaowVg4id5fLZ4g7MBGjWPFiwRmQ4KPyB9fdF2enfF34GKs8uM7WeyU62fT7xX7ZU4EhZDM5ikiu4RejA1uLRQwoT52r",
+      "pass": "x",
+      "keepalive": true,
+      "tls": false,
+      "nicehash": false
+    }
+  ]
+}
